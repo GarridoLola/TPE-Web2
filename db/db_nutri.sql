f85b6ac0483716c7024ba83f683f4113ccf4acb5
Lola Garrido <loolaagarrido@gmail.com>
2025-10-10 19:06:24 -0300

    Correción de mayúsculas

--- db/db_nutri.sql
+++ db/db_nutri.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 19-09-2025 a las 18:43:55
+-- Tiempo de generación: 10-10-2025 a las 23:21:25
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -28,9 +28,9 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `alimentos` (
-  `ID_alimento` int(11) NOT NULL,
+  `id_alimento` int(11) NOT NULL,
   `nombre_alimento` varchar(100) NOT NULL,
-  `ID_grupo` int(11) NOT NULL,
+  `id_grupo` int(11) NOT NULL,
   `descripcion_alimento` varchar(255) NOT NULL,
   `calorias` decimal(6,2) NOT NULL,
   `proteinas` decimal(6,2) NOT NULL,
@@ -47,10 +47,22 @@ CREATE TABLE `alimentos` (
 --
 
 CREATE TABLE `grupos_alimentos` (
-  `ID_grupo` int(11) NOT NULL,
+  `id_grupo` int(11) NOT NULL,
   `nombre_grupo` varchar(50) NOT NULL,
   `descripcion_grupo` text NOT NULL,
   `imagen_grupo` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `nombre_usuario` varchar(50) NOT NULL,
+  `contraseña` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -61,14 +73,20 @@ CREATE TABLE `grupos_alimentos` (
 -- Indices de la tabla `alimentos`
 --
 ALTER TABLE `alimentos`
-  ADD PRIMARY KEY (`ID_alimento`),
-  ADD KEY `ID_grupo` (`ID_grupo`);
+  ADD PRIMARY KEY (`id_alimento`),
+  ADD KEY `ID_grupo` (`id_grupo`);
 
 --
 -- Indices de la tabla `grupos_alimentos`
 --
 ALTER TABLE `grupos_alimentos`
-  ADD PRIMARY KEY (`ID_grupo`);
+  ADD PRIMARY KEY (`id_grupo`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -78,13 +96,19 @@ ALTER TABLE `grupos_alimentos`
 -- AUTO_INCREMENT de la tabla `alimentos`
 --
 ALTER TABLE `alimentos`
-  MODIFY `ID_alimento` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_alimento` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `grupos_alimentos`
 --
 ALTER TABLE `grupos_alimentos`
-  MODIFY `ID_grupo` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_grupo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Restricciones para tablas volcadas
@@ -94,7 +118,14 @@ ALTER TABLE `grupos_alimentos`
 -- Filtros para la tabla `alimentos`
 --
 ALTER TABLE `alimentos`
-  ADD CONSTRAINT `alimentos_ibfk_1` FOREIGN KEY (`ID_grupo`) REFERENCES `grupos_alimentos` (`ID_grupo`);
+  ADD CONSTRAINT `alimentos_ibfk_1` FOREIGN KEY (`ID_grupo`) REFERENCES `grupos_alimentos` (`ID_grupo`),
+  ADD CONSTRAINT `alimentos_ibfk_2` FOREIGN KEY (`id_grupo`) REFERENCES `grupos_alimentos` (`id_grupo`);
+
+--
+-- Filtros para la tabla `grupos_alimentos`
+--
+ALTER TABLE `grupos_alimentos`
+  ADD CONSTRAINT `grupos_alimentos_ibfk_1` FOREIGN KEY (`id_grupo`) REFERENCES `alimentos` (`ID_grupo`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
